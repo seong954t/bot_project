4b1085dee2bf9f112ae97cdf3039d88d77ebaf34
SeoungTae <seong954t@gmail.com>
2017-06-18 14:48:47 +0900

    E_hw_modify

--- chungvis_bot_project/bot_project/bot.py
+++ chungvis_bot_project/bot_project/bot.py
@@ -451,23 +451,9 @@ def handle(msg):
                 run_E("e_info")
 
             elif text == E_hw:
-                try:
-                    send_message(chat_id, ''' 이러닝 홈페이지 과제실 ''')
-                    cursor.execute("SELECT id,title,s_date,e_date,submit FROM e_hw ORDER BY id desc LIMIT 10")
-                    receive_list = []
-                    res = ''
-                    for id,title,s_date,e_date,submit in cursor:
-                        receive_list.append(u"글번호 : %s \n제목 : %s \n시작일 : %s \n종료일 : %s \n제출여부 : %s \n \n\n" % (
-                            id, title, s_date, e_date, submit))
-                    for message in reversed(receive_list):
-                        res += message
-                    send_message(chat_id, res)
-                finally:
-                    cnx.commit()
-                    cnx.close()
-                    time.sleep(3)
-                    help(chat_id)
-                    return
+                send_message(chat_id, ''' 이러닝 홈페이지 과제실 ''')
+                run_E_hw()
+
         elif text in CSE:
             if text == CSE_info:
                 try:
@@ -629,6 +615,25 @@ def handle(msg):
             for id, title, r_date in cursor:
                 receive_list.append(u"글번호 : %s \n제목 : %s \n게시일자 : %s \n\n" % (
                     id, title, r_date))
+            for message in reversed(receive_list):
+                res += message
+            send_message(chat_id, res)
+        finally:
+            cnx.commit()
+            cnx.close()
+            time.sleep(3)
+            help(chat_id)
+            return
+
+    # E_hw
+    def run_E_hw():
+        try:
+            cursor.execute("SELECT id,title,s_date,e_date,submit FROM e_hw ORDER BY id desc LIMIT 10")
+            receive_list = []
+            res = ''
+            for id, title, s_date, e_date, submit in cursor:
+                receive_list.append(u"글번호 : %s \n제목 : %s \n시작일 : %s \n종료일 : %s \n제출여부 : %s \n \n\n" % (
+                    id, title, s_date, e_date, submit))
             for message in reversed(receive_list):
                 res += message
             send_message(chat_id, res)
